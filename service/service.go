a62d54d65aa1a8f49c3ba68ec5a948ab846c3fe8
zhangli2946 <zhangli2946@gmail.com>
2023-12-17 21:44:08 +0800

    introduce service library

---
+++ service/service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+    "context"
+    "sync"
+    
+    "github.com/go4s/util/future"
+)
+
+type (
+    StarterFn func(context.Context, future.Executor) error
+    Starter   interface {
+        Start(context.Context, future.Executor) error
+    }
+)
+
+var (
+    services    []Starter
+    initializer = sync.Once{}
+)
+
+func init() {
+    initializer.Do(initialize)
+}
+
+func initialize() { services = []Starter{} }
+
+func Register(svc Starter) { services = append(services, svc) }
+func Start(ctx context.Context, executor future.Executor) (err error) {
+    for _, svc := range services {
+        if err = svc.Start(ctx, executor); err != nil {
+            return
+        }
+    }
+    return
+}
